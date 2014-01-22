4b407e4c157138674f0261342d233a2199eb75a8
jacoboyen <oyenj@central.edu>
2014-01-22 13:35:29 -0600

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,91 @@
+ module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+		//Optimize images
+		//You may need to run "npm install jpegtran-bin@0.2.0" so that the minimize images feature works properly.
+		imagemin:{
+			dynamic:{
+				files:[{
+					expand:true,
+					cwd: 'images/',
+					src: ['*.{png,jpg,gif}'],
+					dest: 'build/images'
+				}]
+			}
+		},
+		
+		//Combine JS files
+		concat:{
+			js:{
+				src:[
+					'js/*.js',
+				],
+				dest: 'build/js/production.js',
+			},
+			css:{
+				src:[
+						'css/*.css',
+					],
+					dest: 'build/css/production.css',
+			}
+		},
+		
+		// Minify JS file
+		uglify:{
+			build:{
+				src: 'build/js/production.js',
+				dest: 'build/js/production.min.js'
+			}
+		},
+		
+		//Minify CSS
+		cssmin:{
+			minify:{
+				expand: true,
+				cwd: 'build/css/',
+				src: ['*.css', '!*.min.css'],
+				dest: 'build/css/',
+				ext: '.min.css'
+			}
+		},
+		
+		//Clean up build directory
+		clean:["build"],
+		
+		//Watch folders		
+		watch: {
+			images:{
+				files:'images/*.{png,jpg,gif}',
+				tasks:['imagemin'],
+				options:{
+					spawn: false,
+				}
+			},
+			javascripts:{
+				files:'js/*.js',
+				tasks:['concat','uglify'],
+				options:{
+					spawn: false,
+				}
+			},
+			css:{
+				files:'css/*.css',
+				tasks:['concat','cssmin'],
+				options:{
+					spawn: false,
+				}
+			},
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.registerTask('default', ['watch']);
+};
+	
---
+++ readme.md
@@ -0,0 +1,35 @@
+Central Grunt Utility
+===================
+
+Helper utility for developing and optimizing components for the Central College website.
+
+Requirements
+------------------
+* Node.js (http://nodejs.org)
+
+Initial setup
+------------------
+
+* Install nodeJS if you haven't already.
+* Win7 x64 users should run `npm install jpegtran-bin@0.2.0`
+* Run `npm install`
+* Create the following directories:
+** build
+** css
+** images
+** js
+
+Usage
+------------------
+
+Navigate to the directory you cloned this repo to. Run `grunt` at the command line to watch the images, css, and js directories. Adding and removing files will trigger an action that gets exported to the build folder.
+
+Use `grunt clean` to clear out the build directory.
+
+Results
+------------------
+
+* Adding javascripts to the js folder will concatenate and minify all the javascripts in the folder and create a production.js file and production.min.js file.
+* Adding images (.jpg, .png, .gif) to the images folder will optimize the images and dump them in build/images.
+* Adding CSS files to the css folder will result in the files being minified and saved in build/css/production.min.css.
+
